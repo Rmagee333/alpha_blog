d57ccc9b19ce1542718b463d67cbdc7fa98eb552
rileymagee <rmagee122@icloud.com>
2022-04-09 17:44:19 -0400

    add edit temple and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+   
+  def show
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your accout information has been seccessfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,9 +31,11 @@ class UsersController < ApplicationController
     end
   end
 
+
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 
 end
