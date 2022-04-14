0f73ea66679214ff950fd806364b93758b7ef1d2
rileymagee <rmagee122@icloud.com>
2022-04-14 17:05:34 -0400

    added more restrictions based on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
+
   def new
     @user = User.new
   end
@@ -39,6 +43,8 @@ class UsersController < ApplicationController
   end
 
 
+
+
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -47,6 +53,14 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user 
+      flash[:alert] = "you can only edit your own account"
+      redirect_to @user
+    end
+  end
+
 
 
 end
